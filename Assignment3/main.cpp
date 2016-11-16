b1b45285e9de8532651a56b817a38f9830b96b28
郑昊 <zhenghao@d207-023-168-062.wireless.sfu.ca>
2016-11-15 19:42:23 -0800

    Initial Commit

---
+++ Assignment3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment3
+//
+//  Created by 郑昊 on 2016-11-15.
+//  Copyright © 2016 haozheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
